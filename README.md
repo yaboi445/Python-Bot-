568b9acf38b23404f49f4449067b00cabe5dd80f
Quiterion <quiterion12@gmail.com>
2019-03-28 02:15:32 +1100

    Translated text file system to JSON, updated reality bot and readme.

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# Python-Bot-
+# Python-Bot
 
 This cheeky lil boi counts the number of times our teacher says the statement "So the reality is"
 - You can add another reality by typing into discord "?reality", and an extra reality will be added to the reality count
-- You can call back on how many realities there are with "?mh count", and the current number of realities will be displayed
-- The dates that an extra reality the was created can be called back upon on when with "?date + [date you want to find]", and the number of realities that were added on that day will be added.
+- You can call back on how many realities there are with "?r count", and the current number of realities will be displayed
+- The dates that an extra reality the was created can be called back upon on when with "?r date + [date you want to find]", and the number of realities that were added on that day will be added.
 
-The file counter.txt holds the value for the reality count so that it is not lost each time to program stops running.
-The file date.txt holds that data that eah reality was created.
+The file counter.json holds the value for the reality count per each day so that it is not lost each time to program stops running.
---
+++ reality_bot.py
@@ -0,0 +1,41 @@
+import discord
+import datetime # for finding date and time
+import json
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    user_input = message.content
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+    with open('counter.json', 'r') as file:
+        json_file = json.loads(file)
+
+    date_time = str(datetime.date.today())
+    if user_input.startswith("?reality"):
+        count = json_file.get(date_time, 0) + 1
+        json_file[date_time] = count
+        reply = ('Approximately `' + str(count) + '` parallel temporal anomalies have been generated during this rotational period.').format(message)
+        await client.send_message(message.channel, reply) # sends message back
+        with open('counter.json', 'r') as file:
+            file.write(json.dumps(json_file))
+
+    elif user_input.startswith("?r count"):
+        reply = ('Current number of parallel temporal realities is: `' + json_file.get(date_time, 0) + '`').format(message)
+        await client.send_message(message.channel, reply)
+
+    elif user_input.startswith("?r date"):
+        date_value = json_file.get(user_input[6:], "[### ERROR: UNCONSTRAINED REALITIES. TEMPORAL COLLAPSE IMMINENT ###]")
+        reply = ("On the " + user_input[6:] + " there were " + str(date_value) + " realities").format(message)
+        await client.send_message(message.channel, reply)
+        # dispays the current number of realities
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run('client_token for bot')
