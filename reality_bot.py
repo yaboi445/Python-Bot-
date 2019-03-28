17cc3623584de731ffe5435cc5080527e884f68d
Quiterion <quiterion12@gmail.com>
2019-03-28 19:01:43 +1100

    Bug fixes

--- reality_bot.py
+++ reality_bot.py
@@ -10,24 +10,24 @@ async def on_message(message):
     if (message.author == client.user) or (not user_input.startswith('?r')):
         return
     with open('counter.json', 'r') as file:
-        json_file = json.loads(file)
+        json_file = json.loads(file.read())
 
     date_time = str(datetime.date.today())
     if user_input.startswith("?reality"):
         count = json_file.get(date_time, 0) + 1
         json_file[date_time] = count
-        reply = ('Approximately `' + str(count) + '` parallel temporal anomalies have been generated during this rotational period.').format(message)
+        reply = ('Anomaly logged and noted. Approximately `' + str(count) + '` parallel temporal anomalies have now been generated during this rotational period.').format(message)
         await client.send_message(message.channel, reply) # sends message back
-        with open('counter.json', 'r') as file:
+        with open('counter.json', 'w') as file:
             file.write(json.dumps(json_file))
 
     elif user_input.startswith("?r count"):
-        reply = ('Current number of parallel temporal realities is: `' + str(sum(list(json_file.values()))) + '`').format(message)
+        reply = ('Current global number of parallel realities: `' + str(sum(list(json_file.values()))) + '`').format(message)
         await client.send_message(message.channel, reply)
 
     elif user_input.startswith("?r date"):
-        date_value = json_file.get(user_input[6:], "[### ERROR: UNCONSTRAINED REALITIES. TEMPORAL COLLAPSE IMMINENT ###]")
-        reply = ("On the " + user_input[6:] + " there were " + str(date_value) + " realities").format(message)
+        date_value = json_file.get(user_input[8:], "[ERROR: UNCONSTRAINED REALITIES. TEMPORAL COLLAPSE IMMINENT]")
+        reply = ("On the " + user_input[8:] + " there were `" + str(date_value) + "` realities generated.").format(message)
         await client.send_message(message.channel, reply)
         # dispays the current number of realities
 
